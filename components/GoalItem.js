8dcb64f243f5f4754465911ed0acbe0cb9370aa4
devmev10 <ravitokas90@gmail.com>
2023-01-18 16:14:16 +0530

    Padding update on item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
     <View style={styles.GoalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable
+        android_ripple={{ color: "green" }}
+        onPress={() => props.onDeleteItem(props.id)}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -12,11 +17,12 @@ export default GoalItem;
 const styles = StyleSheet.create({
   GoalItem: {
     margin: 8,
-    padding: 8,
+
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
